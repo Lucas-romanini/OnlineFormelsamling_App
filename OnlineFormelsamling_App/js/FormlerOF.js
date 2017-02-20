10564ac4d9e853e5050fdf66cf4367caac460afb
Lucas-romanini <lucas@romanini.dk>
2017-02-20 12:54:40 +0100

    appen

---
+++ OnlineFormelsamling_App/js/FormlerOF.js
@@ -0,0 +1,12 @@
+function FormlerOF(ID, Navn, Beskrivelse, IMG, Parameter1, Parameter2, Parameter3, Parameter4, Parameter5, Parameter6) {
+    this.ID = id;
+    this.Navn = navn;
+    this.Beskrivelse = beskrivelse;
+    this.IMG = IMG;
+    this.Parameter1 = Parameter1;
+    this.Parameter2 = Parameter2;
+    this.Parameter3 = Parameter3;
+    this.Parameter4 = Parameter4;
+    this.Parameter5 = Parameter5;
+    this.Parameter6 = Parameter6;
+}
---
+++ OnlineFormelsamling_App/js/Main.js
@@ -0,0 +1,11 @@
+var URL = "http://localhost:28114/";
+
+
+function navigate(id, page) {
+    if (id > 0) {
+        window.localStorage.setItem("id", id);
+    }
+    Window.location.assign(page + ".html");
+}
+
+
